82b3c098f9b71a4f8c6b33e2040639b4d302a064
Shiogui Nagasse <shiogui@gmail.com>
2026-05-07 20:29:55 -0300

    now it's better

--- CrossVB6/IDE.cs
+++ CrossVB6/IDE.cs
@@ -7,7 +7,7 @@ public class IDE
     internal void UpdateAndDraw()
     {
         Raylib.ClearBackground(Color.White);
-        Raylib.DrawText("HOT RELOAD BABY!", 190, 200, 20, Color.DarkGray);
-        Raylib.DrawText("Press F11 to toggle fullscreen", 10, 40, 10, Color.DarkGray);
+        Raylib.DrawText("HOT RELOAD BABY - SIMPLE!", 190, 200, 20, Color.DarkGray);
+        // Raylib.DrawText("Press F11 to toggle fullscreen", 10, 40, 10, Color.DarkGray);
     }
 }
--- dev.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
-dotnet watch run --project "$SCRIPT_DIR/CrossVB6.Host/CrossVB6.Host.csproj"
